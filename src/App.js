ad6506d917ee361e0f73c62b2aa2683661b2a8aa
luciano plaza <lucianomps2015@gmail.com>
2022-08-05 19:32:51 -0300

    Install

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Routes } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <Routes>
+      {/* <Route path="/" element={}/>
+      <Route path="/" element={}/> */}
+    </Routes>
+  );
+}
+
+export default App;
