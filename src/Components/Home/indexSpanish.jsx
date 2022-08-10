bd0005b3633446b42e48d36aeb23f8c9829c979d
luciano plaza <lucianomps2015@gmail.com>
2022-08-09 22:52:59 -0300

    Pruebas 0.1

--- src/Components/Home/indexSpanish.jsx
+++ src/Components/Home/indexSpanish.jsx
@@ -1,10 +1,13 @@
 // import { useState, useEffect } from "react";
+import YourJob from "../../Sensible information/imagePF1.jpeg";
+import PI_Foodies from "../../Sensible information/PI-Foodies.jpg";
 import projects from "../../Sensible information/Projects.json";
 import Project from "../Project/index.jsx";
 import Nav from "../Nav/index.jsx";
 import style from "./Home.module.css";
 
 export default function HomeSpanish() {
+  const Images = [YourJob, PI_Foodies];
   return (
     <>
       <Nav />
@@ -14,7 +17,7 @@ export default function HomeSpanish() {
             <div key={project.id} className={style.project}>
               <Project
                 Title={project.title}
-                image={project.img}
+                image={Images.find(img__name => img__name === project.title)}
                 repo={project.repository}
                 deploy={project.deployment}
                 description={project.description}
