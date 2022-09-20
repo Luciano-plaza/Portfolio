e8b3484ca2b828ee226c90c594cc9c85d288d797
luciano plaza <lucianomps2015@gmail.com>
2022-09-20 20:14:14 -0300

    feat projects

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -4,6 +4,7 @@ import style from "./Footer.module.css";
 export default function Footer() {
   return (
     <div className={style.footer__container} id="contacto">
+      <h3 className={style.footer__title}>Hablemos !</h3>
       <Mailer />
     </div>
   );
--- src/Components/Projects/index.jsx
+++ src/Components/Projects/index.jsx
@@ -1,73 +1,83 @@
 import style from "./Projects.module.css";
+import Foodies from "../../Images/PI-Foodies.png";
+import YourJob from "../../Images/PF-YourJob.jpeg";
 
 export default function Projects() {
   return (
-    <div className={style.projects__container} id="projects">
-      <h2>Projects</h2>
-      <div className={style.project}>
-        <h3 className={style.project__title}>Foodies</h3>
-        <p className={style.project__description}>
-          Este es un proyecto individial que está hecho sólo con React, Redux,
-          Node.js, y sequelize(PostgreSQL) y trabaja con la API(Applicacion Page
-          Interface) spoonacular. Esta pagina cuenta con las siguentes
-          funcionalidades sin llegar a ser CRUD(Create Read Update Delete),
-          todavía. La API tiene datos sobre platos de comida con muchos más
-          detalles que no eran relevantes en mi caso.
-        </p>
-        <h4 className={style.project__title__list}>Funcionalidades</h4>
-        <ul className={style.project__list}>
-          <li>Buscar recetas</li>
-          <li>
-            Filtrardas por dietas y Ordenadas alfabéticamente y por máxima y
-            mínima puntuación de salud
-          </li>
-          <li>Crear recetas propias</li>
-          <li>Resetear las búsquedas y filtrados</li>
-          <li>
-            Paginado acorde a la cantidad de recetas encontradas en el caso de
-            búsqueda
-          </li>
-        </ul>
-        <button className={style.project__link}>
+    <div className={style.projects__transform}>
+      <div className={style.projects__container} id="projects">
+        <h2>Projects</h2>
+        <div className={style.project}>
+          <img src={Foodies} alt="Foodies" className={style.project__img} />
+          <h3 className={style.project__title}>Foodies</h3>
+          <p className={style.project__description}>
+            Este es un proyecto individial que está hecho sólo con React, Redux,
+            Node.js, y sequelize(PostgreSQL) y trabaja con la API(Applicacion
+            Page Interface) spoonacular. Esta pagina cuenta con las siguentes
+            funcionalidades sin llegar a ser CRUD(Create Read Update Delete),
+            todavía. La API tiene datos sobre platos de comida con muchos más
+            detalles que no eran relevantes en mi caso.
+          </p>
+          <h4 className={style.project__title__list}>Funcionalidades</h4>
+          <ul className={style.project__list}>
+            <li>Buscar recetas</li>
+            <li>
+              Filtrardas por dietas y Ordenadas alfabéticamente y por máxima y
+              mínima puntuación de salud
+            </li>
+            <li>Crear recetas propias</li>
+          </ul>
           <a
+            className={style.project__link}
             href="https://github.com/Luciano-plaza/PI-FOOD"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Repositorio
+            <button>Código</button>
           </a>
-        </button>
-        <button className={style.project__link}>
           <a
+            className={style.project__link}
             href="https://pi-food-liard.vercel.app"
             target="_blank"
             rel="noopener noreferrer"
           >
-            App
+            <button>App</button>
           </a>
-        </button>
-      </div>
-      <div className={style.project}>
-        <label className={style.project__title}>Your Job</label>
-        <p className={style.project__description}></p>
-        <button className={style.project__link}>
+        </div>
+        <div className={style.project}>
+          <img src={YourJob} alt="YourJob" className={style.project__img} />
+          <h3 className={style.project__title}>Your Job</h3>
+          <p className={style.project__description}>
+            Este es un proyecto grupal que está hecho con React, Redux, Express,
+            sequelize y microservicios como Auth0, UploadCare.
+          </p>
+          <h4 className={style.project__title__list}>Funcionalidades</h4>
+          <ul className={style.project__list}>
+            <li>Postear en el foro</li>
+            <li>Colgar anuncios de trabajos para contactar con las personas</li>
+            <li>Contactar con empresas o personas por chat</li>
+            <li>
+              Pagar un premium para obtener beneficios en las búsquedas de
+              empresas
+            </li>
+          </ul>
           <a
+            className={style.project__link}
             href="https://github.com/Luciano-plaza/Your-Job"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Repositorio
+            <button>Código</button>
           </a>
-        </button>
-        <button className={style.project__link}>
           <a
+            className={style.project__link}
             href="https://your-job-seven.vercel.app"
             target="_blank"
             rel="noopener noreferrer"
           >
-            App
+            <button>App</button>
           </a>
-        </button>
+        </div>
       </div>
     </div>
   );
